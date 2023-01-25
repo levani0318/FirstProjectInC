4e3ca00690158c6d4e1efe3f0f830fdf3e163570
levani0318 <levani0318@gmail.com>
2023-01-26 01:21:31 +0400

    FirstProject

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char leter='T';
+    int numb=100;
+    cout<<leter<<endl;
+    cout<<numb<<endl;
+    
+    float whole=10.10;
+    double twin=50.23;
+    cout<<whole<<endl;
+    cout<<twin<<endl;
+    
+    int array[5]={5,10,15,20,30};
+    int sum=array[0]+array[1]+array[2]+array[3]+array[4];
+    int aver=(array[0]+array[1]+array[2]+array[3]+array[4])/5;
+    cout<<sum<<endl;
+    cout<<aver<<endl;
+
+    return 0;
+}
